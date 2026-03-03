81f7a96f7e18cde5fa0043793408d7fe70406f61
Na22eR <Nasser.Yacine-Account@ProtonMail.com>
2026-03-03 17:26:40 +0100

    Initial Commit

---
+++ src/main/java/net/na22er/content_calender/ContentCalenderApplication.java
@@ -0,0 +1,15 @@
+package net.na22er.content_calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCalenderApplication.class, args);
+
+		System.out.println("Hallo Welt");
+	}
+
+}
